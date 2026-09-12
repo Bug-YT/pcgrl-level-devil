1c919879f42932a93d0ccd7b4dbcca62065da2c9
Bug-YT <Bug.YT@icloud.com>
2026-09-12 17:12:44 +0200

    Remove all files related to the PCGRL Level Devil pipeline, including configurations, environments, and utility scripts.

---
+++ start.sh
@@ -0,0 +1,20 @@
+printf '%s\n' ""
+
+printf '%s\n' "Creating Python virtual environment..."
+python -m venv ./venv && printf '%s\n' "Virtual environment was successfully created." || printf '%s\n' "An error occurred while creating the virtual environment."
+
+printf '%s\n' "Activating virtual environment..."
+if . ./venv/bin/activate; then
+    printf '%s\n' "Virtual environment was successfully initialized."
+else
+    printf '%s\n' "An error occurred while initializing the virtual environment."
+fi
+
+printf '%s\n' "Installing required packages..."
+if python -m pip install -r ./requirements.txt; then
+    printf '%s\n' "Required packages were successfully installed."
+else
+    printf '%s\n' "An error occurred while installing the required packages."
+fi
+
+printf '%s\n' "Please run '' if you want to start the project."
